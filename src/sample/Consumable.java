003299a301b766eb708f085bede8e4e20916ead3
Travis Lange <travislange12@gmail.com>
2019-03-17 14:52:47 -0400

    Moved a method in order to save space

--- src/sample/Consumable.java
+++ src/sample/Consumable.java
@@ -19,14 +19,6 @@ public abstract class Consumable extends StockItem {
 
     }
 
-    @Override
-    public String getItemTypeDetails() {
-        String details = "\nDetails:\n";
-        details += "Expiration Date: " + expirationDate + "\n";
-        details += "Calorie Count: " + calorieCount + "\n";
-        return details;
-    }
-
     public Consumable(String expirationDate, double calorieCount, String name, double price, boolean isOnSale, String itemDescription, String itemCategory) {
         super(name, price, isOnSale, itemDescription, itemCategory);
         this.expirationDate = expirationDate;
@@ -47,6 +39,14 @@ public abstract class Consumable extends StockItem {
 
     public void setCalorieCount(double calorieCount) {
         this.calorieCount = calorieCount;
+    }
+
+    @Override
+    public String getItemTypeDetails() {
+        String details = "\nDetails:\n";
+        details += "Expiration Date: " + expirationDate + "\n";
+        details += "Calorie Count: " + calorieCount + "\n";
+        return details;
     }
 
     @Override
