5d619a8fc82a09ca2996c73623431d233f5bcda0
Travis Lange <travislange12@gmail.com>
2019-03-17 14:47:09 -0400

    Fixed bug where the class computer was marked as a consumable

--- src/sample/Computer.java
+++ src/sample/Computer.java
@@ -12,7 +12,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public abstract class Computer extends NonConsumable {
+public class Computer extends NonConsumable {
 
     protected String processor;
     protected String memory;
